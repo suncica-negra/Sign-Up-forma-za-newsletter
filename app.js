21f76ee673a143147990319511dece52ddd81670
Dea K <52324941+suncica-negra@users.noreply.github.com>
2020-07-04 15:36:30 +0200

    SEO optimized

--- app.js
+++ app.js
@@ -4,53 +4,54 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
-	var firstName  = req.body.name;
-	var lastName  = req.body.last;
-	var email  = req.body.email;
-
+app.post("/", function (req, res) {
+	var firstName = req.body.name;
+	var lastName = req.body.last;
+	var email = req.body.email;
 	var data = {
-		members: [
-			{email_address: email,
+		members: [{
+			email_address: email,
 			status: "subscribed",
 			merge_fields: {
 				FNAME: firstName,
 				LNAME: lastName
-			}}
-		]
+			}
+		}]
 	};
-
 	var jsonData = JSON.stringify(data);
-	
 	var options = {
-		url: "https://us3.api.mailchimp.com/3.0/lists/ebcede202b",
+		url: "https://your-url.com",
 		method: "POST",
 		headers: {
-			"Authorization": ""
+			"Authorization": "your-auth"
 		},
 		body: jsonData
 	};
-
-request(options, function(error, response, body){
-	if (error){
-		res.sendFile(__dirname + "/failure.html");
-	} else { if (response.statusCode === 200) {res.sendFile(__dirname + "/success.html");
-	} else {
-		res.sendFile(__dirname + "/failure.html");
-	}
-}});
+	request(options, function (error, response, body) {
+		if (error) {
+			res.sendFile(__dirname + "/failure.html");
+		} else {
+			if (response.statusCode === 200) {
+				res.sendFile(__dirname + "/success.html");
+			} else {
+				res.sendFile(__dirname + "/failure.html");
+			}
+		}
+	});
 });
 
-app.post("/failure", function(req, res){
+app.post("/failure", function (req, res) {
 	res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Server is running.");
 });
